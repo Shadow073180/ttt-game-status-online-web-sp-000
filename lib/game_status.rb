a0bb5e6e98b7b34054f02a1861036179012d9b4b
David masterson <david73180@gmail.com>
2018-09-30 19:57:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,8 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def won?(board)
   i = 1
-  WIN_COMBINATIONS.each do |x|
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |x|
   win_i = [x[0], x[1], x[2]]
   i += 1
 end
