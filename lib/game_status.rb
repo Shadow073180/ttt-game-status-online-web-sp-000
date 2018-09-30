6f70f33d0690d4a49e24d1cbd291a1403c13b3fb
David masterson <david73180@gmail.com>
2018-09-30 19:39:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,8 +63,9 @@ def won?(board)
       return win_index_7
     elsif (board[position_22] == "X" && board[position_23] == "X" && board[position_24] == "X") || (board[position_22] == "O" && board[position_23] == "O" && board[position_24] == "0")
       return win_index_8.to_a
-    else
+    elsif draw?(board)
       return false
+      
   end
 end
 
